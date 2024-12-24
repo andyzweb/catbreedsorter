f575f500412b780a8faf90d4b0c4674f9823e797
Andrew Euell <andyzweb@hexagon.local>
2024-12-24 12:11:45 -0500

    push push push:

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -9,6 +9,7 @@ export const setLocalStorage = (key: string, value: any) => {
 export const getLocalStorage = (key: string, defaultValue: any) => {
   try {
     const value = localStorage.getItem(key);
+    //Comment in here to trigger new build
     return value ? JSON.parse(value) : defaultValue;
   } catch (error) {
     console.error('Error reading from localStorage:', error);
