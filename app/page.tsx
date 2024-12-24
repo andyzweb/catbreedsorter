dd573a30449204ae67b070b78737824b6b38d971
Andrew Euell <andyzweb@hexagon.local>
2024-12-24 12:04:03 -0500

    this needs a root element

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import CatBreedSorter from '../components/CatBreedSorter';
 
 export default function Home() {
   return (
+    <html>
     <main className="min-h-screen bg-gray-100">
       <CatBreedSorter />
     </main>
+    </html>
   );
 }
 
